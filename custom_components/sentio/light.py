6443e2caf04003deaac5c1a8ee5611bdebb35872
Åke Strandberg <ake@strandberg.eu>
2021-10-31 16:13:46 +0100

    Use _attr_ style in Light

--- custom_components/sentio/light.py
+++ custom_components/sentio/light.py
@@ -29,20 +29,15 @@ class SaunaLight(LightEntity):
     """Representation of a light."""
 
     def __init__(self, hass, entry):
-        """Initialize the sensor."""
+        """Initialize the light entity."""
         self._entryid = entry.entry_id
         self._api = hass.data[DOMAIN][entry.entry_id]
-        self._unique_id = DOMAIN + "_" + "saunalight"
-
-    @property
-    def device_info(self):
-        return DeviceInfo(
-            identifiers={(DOMAIN, "4321")},
-        )
-
-    @property
-    def should_poll(self):
-        return False
+        self._attr_unique_id = DOMAIN + "_" + "saunalight"
+        self._attr_device_info = DeviceInfo(identifiers={(DOMAIN, "4321")})
+        self._attr_should_poll = False
+        self._attr_name = "Sauna Light"
+        self._attr_is_on = self._api.light_is_on
+        self._attr_brightness = int(50 * 2.55)
 
     async def async_added_to_hass(self):
         """Register callbacks."""
@@ -55,25 +50,11 @@ class SaunaLight(LightEntity):
         self.async_schedule_update_ha_state(True)
 
     @property
-    def name(self):
-        """Return the name of the light."""
-        return "Sauna Light"
-
-    @property
-    def unique_id(self):
-        """Return the ID of this device."""
-        return self._unique_id
-
-    @property
     def supported_features(self):
         feat = 0
         if self._api.config("light dimming") == "on":
             feat = feat | SUPPORT_BRIGHTNESS
         return feat
-
-    @property
-    def is_on(self):
-        return self._api.light_is_on
 
     async def async_turn_on(self, **kwargs):
         _LOGGER.debug(self.name + " Turn_on")
@@ -84,10 +65,6 @@ class SaunaLight(LightEntity):
         _LOGGER.debug(self.name + " Turn_off")
         self._api.set_light(PYS_STATE_OFF)
         self.async_schedule_update_ha_state(True)
-
-    @property
-    def brightness(self):
-        return int(50 * 2.55)
 
     async def async_update(self):
         return
