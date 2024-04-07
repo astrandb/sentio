50ad4f357b056ca86805fab1f05244829503f00f
Åke S - Piper <ake@strandberg.eu>
2024-04-07 13:48:45 +0000

    Bump version to 2024.4.0

--- custom_components/sentio/const.py
+++ custom_components/sentio/const.py
@@ -1,7 +1,7 @@
 """Constants for the sentio sauna integration."""
 
 DOMAIN = "sentio"
-VERSION = "2024.3.0"
+VERSION = "2024.4.0"
 SIGNAL_UPDATE_SENTIO = "update_sentio"
 MANUFACTURER = "Sentiotec"
 DEVICE_NAME = "Sentio Pro Sauna Controller"
